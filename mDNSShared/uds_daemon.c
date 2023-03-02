5e1de368320abf0bdbf1fe888dfd7915341b1c05
SkyFi Geek <45924209+mobileskyfi@users.noreply.github.com>
2023-03-02 11:54:08 -0800

    do not use __block in POSIX

--- mDNSShared/uds_daemon.c
+++ mDNSShared/uds_daemon.c
@@ -2912,7 +2912,11 @@ exit:
 mDNSlocal mStatus add_domain_to_browser(request_state *info, const domainname *d)
 {
     browser_t *b, *p;
+#ifdef __BLOCKS__
     __block mStatus err;
+#else
+    mStatus err;
+#endif
 
     for (p = info->u.browser.browsers; p; p = p->next)
     {
